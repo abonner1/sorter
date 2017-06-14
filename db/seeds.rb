f0969dd37b560e08e0ec6c0cbf0a883e5462491e
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-14 15:15:43 -0400

    Fix tests.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+first_user = User.create(
+      name: "Andrew",
+      username: "andyb000",
+      email: "andyb000@gmail.com",
+      password: "password")
+
+second_user = User.create(
+      name: "Avi",
+      username: "aviflombaum",
+      email: "aviflombaum@flatiron.com",
+      password: "password")
+
+ruby_on_rails = Language.create(name: "Ruby on Rails")
+
+new_topic = Topic.create(name: "Associations")
+
+new_resource = first_user.resources.build(
+      title: "Active Record Associations",
+      url: "http://guides.rubyonrails.org/association_basics.html",
+      description: "This is a resource!")
+
+new_resource.language = ruby_on_rails
+new_resource.topics << new_topic
+new_resource.save
--- spec/models/resource_topic_spec.rb
+++ spec/models/resource_topic_spec.rb
@@ -4,15 +4,14 @@ RSpec.describe ResourceTopic, type: :model do
   before(:each) do
     @resource = Resource.first
     @topic = Topic.first
-    @resource_topic = ResourceTopic.create(resource_id: @resource.id, topic_id: @topic.id)
   end
 
   it "belongs to topic" do
-    expect(@resource_topic.resource).to be_a(Resource)
+    expect(@topic.resources.first).to be_a(Resource)
   end
 
   it "belongs to resource" do
-    expect(@resource_topic.topic).to be_a(Topic)
+    expect(@resource.topics.first).to be_a(Topic)
   end
 
 end
