53007d35f382f78a6a3195bc90ed830db9d6b250
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-14 14:27:29 -0400

    Add resources.

---
+++ db/migrate/20170614182538_create_resources.rb
@@ -0,0 +1,13 @@
+class CreateResources < ActiveRecord::Migration[5.1]
+  def change
+    create_table :resources do |t|
+      t.string :title
+      t.string :url
+      t.text :description
+      t.integer :user_id
+      t.integer :language_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614181832) do
+ActiveRecord::Schema.define(version: 20170614182538) do
 
   create_table "languages", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "resources", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.text "description"
+    t.integer "user_id"
+    t.integer "language_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
