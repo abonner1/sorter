cdd2b599100420841a913eb13e93272b9b4b782d
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-14 14:19:42 -0400

    Add topics table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614180927) do
+ActiveRecord::Schema.define(version: 20170614181832) do
 
   create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
