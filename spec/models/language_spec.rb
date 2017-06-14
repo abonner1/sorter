a03957e882a0e948c71d9044e058c96db643f8e8
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-14 15:26:59 -0400

    Fix test.

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Language, type: :model do
-  let(:language) do
-    Language.create(name: "Ruby")
+  before(:each) do
+    @language = Language.first
   end
 
   it "validates with a name" do
-    expect(language).to be_valid
+    expect(@language).to be_valid
   end
 
   it "does not validate without a name" do
-    language.name = nil
-    expect(language).not_to be_valid
+    @language.name = nil
+    expect(@language).not_to be_valid
   end
 
   it "has many resources" do
-    language.resources.build
-    expect(language.resources.count).to eq(1)
+    @language.resources.build
+    expect(@language.resources.count).to eq(1)
   end
 
 end
