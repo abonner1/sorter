c018e314be22a305809a9b3d3dc05cf9a1774ec1
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-14 15:16:24 -0400

    Fix.

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  let(:topic) do
-    Topic.create(name: "Authentications")
+  before(:each) do
+    @topic = Topic.first
   end
 
   it "it validates with presence of name" do
-    expect(topic).to be_valid
+    expect(@topic).to be_valid
   end
 
   it "does not validate if there is no name" do
-    topic.name = nil
-    expect(topic).not_to be_valid
+    @topic.name = nil
+    expect(@topic).not_to be_valid
   end
 
   it "has many resources" do
-    topic.resources.build
-    expect(topic.resources.count).to eq(1)
+    @topic.resources.build
+    expect(@topic.resources.count).to eq(1)
   end
 
 end
