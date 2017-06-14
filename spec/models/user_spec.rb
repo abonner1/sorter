20441a2d689ffc57ccc0dd189c916c987c719d8a
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-14 15:34:40 -0400

    Add validation tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,22 @@ RSpec.describe User, type: :model do
     @user = User.first
   end
 
+  describe "username" do
+    it "is unique" do
+      User.create(
+            name: "Avi",
+            username: "aviflombaum",
+            email: "aviflombaum2@flatiron.com",
+            password: "password")
+      expect(User.where(username: "aviflombaum").count).to eq(1)
+    end
+
+    it "validates user when present" do
+      @user.username = nil
+      expect(@user).not_to be_valid
+    end
+  end
+
   it "has many resources" do
     expect(@user.resources.count).to eq(1)
   end
