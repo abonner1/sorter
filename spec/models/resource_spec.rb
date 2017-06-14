d81924806984b2a30e2e449db4e98f1455f885d4
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-14 15:11:50 -0400

    Fix tests.

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,40 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Resource, type: :model do
-  let(:resource) do
-    Resource.new(
-        title: "Active Record Associations",
-        url: "http://guides.rubyonrails.org/association_basics.html",
-        description: "This is a resource!",
-        user_id: User.first.id,
-        language_id: Language.first.id)
+  before(:each) do
+    @resource = Resource.first
   end
 
   describe "attributes" do
     it "validates with a title and a url" do
-      expect(resource).to be_valid
+      expect(@resource).to be_valid
     end
 
     it "does not validate without a title or a url" do
-      resource.title = nil
-      resource.url = nil
-      expect(resource).not_to be_valid
+      @resource.title = nil
+      @resource.url = nil
+      expect(@resource).not_to be_valid
     end
 
     it "has a description" do
-      expect(resource.description).to eq("This is a resource!")
+      expect(@resource.description).to eq("This is a resource!")
     end
 
     it "validates with a language_id and a user_id" do
-      expect(resource).to be_valid
+      expect(@resource).to be_valid
     end
 
     it "does not validate without a language_id or a user_id" do
-      resource.language_id = nil
-      resource.user_id = nil
-      expect(resource).not_to be_valid
+      @resource.language_id = nil
+      @resource.user_id = nil
+      expect(@resource).not_to be_valid
+    end
+  end
+
+  describe "assocations" do
+    it "has many topics" do
+      expect(@resource.topics).to eq(1)
     end
 
+    it "belongs to a user" do
+      expect(@resource.user).to be_a(User)
+    end
+
+    it "belongs to a language" do
+      expect(@resource.language).to be_a(Language)
+    end
   end
 
 end
