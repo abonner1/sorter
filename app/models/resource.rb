ece80cb516f06094f62c87ebd5ee2894ff7af6c8
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-14 15:29:57 -0400

    Pass tests.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,9 @@
 class Resource < ApplicationRecord
+  belongs_to :user
   belongs_to :language
   has_many :resource_topics
   has_many :topics, through: :resource_topics
+
+  validates :title, presence: true
+  validates :url, presence: true
 end
