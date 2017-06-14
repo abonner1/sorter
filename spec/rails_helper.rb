1adfd2524ef8157d4cc844973658624964f0d746
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-14 13:56:42 -0400

    Configure rspec with database_cleaner.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,8 +27,24 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
+    load "#{Rails.root}/db/seed.rb"
+  end
+
+  config.before(:eash, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
