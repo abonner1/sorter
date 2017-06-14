1ae0ab153f2ada3028909358623c627c8dd7e041
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-14 14:55:27 -0400

    Pass tests.

--- spec/models/resource_topic_spec.rb
+++ spec/models/resource_topic_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ResourceTopic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @resource = Resource.first
+    @topic = Topic.first
+    @resource_topic = ResourceTopic.create(resource_id: @resource.id, topic_id: @topic.id)
+  end
+
+  it "belongs to topic" do
+    expect(@resource_topic.resource).to be_a(Resource)
+  end
+
+  it "belongs to resource" do
+    expect(@resource_topic.topic).to be_a(Topic)
+  end
+
 end
