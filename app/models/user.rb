9f7447110b4f7021092ea82d8c5ea3471f94dac1
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-14 15:21:18 -0400

    Add association to resources.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :validatable
+
+  has_many :resources
 end
