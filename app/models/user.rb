f90ad25a1c0b471682eeecef874e6e889a39da87
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-15 14:54:42 -0400

    Add omniauth and validate omniauth signin.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
+  devise :omniauthable, omniauth_providers: [:github]
 
   has_many :resources
 
   validates :username, presence: true
   validates :username, uniqueness: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.username = auth.info.nickname
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
