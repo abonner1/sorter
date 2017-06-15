bc7a94a6262b53f1bb52a46a9869b883e7f29d3f
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-15 16:37:26 -0400

    Migrate all.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615202259) do
+ActiveRecord::Schema.define(version: 20170615203102) do
+
+  create_table "course_resources", force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
