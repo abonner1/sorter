ec680bf7702247910eeaf62b4c08229905a2938a
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-15 09:50:38 -0400

    Add favorited.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614185825) do
+ActiveRecord::Schema.define(version: 20170615134955) do
 
   create_table "languages", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170614185825) do
     t.integer "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "favorited"
   end
 
   create_table "resources", force: :cascade do |t|
