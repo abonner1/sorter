1af7f5f6bc8272de89dd9bb56eabd6b73c44dd74
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-15 18:36:50 -0400

    Add correct_user method as controller method and helper.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, except: [:home]
 
+  helper_method :correct_user
+
+  def correct_user
+    !!(current_user == @resource.user)
+  end
+
   protected
 
     def configure_permitted_parameters
