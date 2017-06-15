14d4d8165e9585aafa9fe184c313d5f137f5bfda
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-15 16:53:50 -0400

    Draw basic relationship.

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,7 @@ class Course < ApplicationRecord
   has_many :users, through: :enrolled_courses
   has_many :course_resources
   has_many :resources, through: :course_resources
+  belongs_to :language
 
   validates :title, presence: true
   validates :title, uniqueness: true
--- app/models/language.rb
+++ app/models/language.rb
@@ -1,5 +1,6 @@
 class Language < ApplicationRecord
   has_many :resources
+  has_many :courses
 
   validates :name, presence: true
   validates :name, uniqueness: true
