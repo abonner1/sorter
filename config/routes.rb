bbf54aa95bbb6f75809297c6736567b8a8fcc17b
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-15 14:53:34 -0400

    Add omniauth route and config.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
 
   resources :users, only: [:show] do
     resources :resources, only: [:index]
