063c0152e8c0d6dd4eeeeadcd71bf76bb816149c
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-15 16:41:09 -0400

    Validate course.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,10 @@
 class Course < ApplicationRecord
   has_many :enrolled_courses
   has_many :users, through: :enrolled_courses
-
   has_many :course_resources
   has_many :resources, through: :course_resources
+
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :description, presence: true
 end
