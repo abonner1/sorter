c604d6f6ea8106549d363331bd3956c4547e108b
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-15 14:14:49 -0400

    Change root route. Add nested users resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'resources#index'
+  root 'welcome#home'
   devise_for :users
+
+  resources :users, only: [:show] do
+    resources :resources, only: [:index]
+  end
+
   resources :resources
 end
