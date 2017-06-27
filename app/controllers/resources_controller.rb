62806e3fe08a1173dfb5b1c98d273551b20a758b
Andrew Bonner <andrewbonner92@gmail.com>
2017-06-27 15:31:41 -0400

    Pass current_user argument to add_user method in resource controller.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -10,7 +10,7 @@ class ResourcesController < ApplicationController
 
   def create
     @resource = Resource.new(resource_params)
-    @resource.add_user
+    @resource.add_user(current_user)
     if @resource.save
       redirect_to root_path
     else
