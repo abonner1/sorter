da8b7754fa74530b0d252fe69b971f746222a2fa
Andrew Bonner <andrewbonner92@gmail.com>
2017-07-23 15:51:13 -0400

    Make home controller restful.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#home'
+  root 'home#index'
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
 
   resources :users, only: [:show] do
