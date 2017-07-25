fcd698149178a42fe1cd82c93d49ae72810c066f
Andrew Bonner <andrewbonner92@gmail.com>
2017-07-25 15:38:02 -0400

    Display language in resource serializer.

--- app/serializers/resource_serializer.rb
+++ app/serializers/resource_serializer.rb
@@ -1,3 +1,4 @@
 class ResourceSerializer < ActiveModel::Serializer
-  attributes :id, :title, :url, :description, :user_id, :language_id
+  attributes :id, :title, :url, :description, :user_id
+  has_many :languages
 end
