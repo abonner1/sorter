cc015c96ef02c6458fe302c2f53bb5ffe1385907
Andrew Bonner <andrewbonner92@gmail.com>
2017-07-25 12:36:24 -0400

    Create action not yet working for Comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,17 @@ class CommentsController < ApplicationController
     @comment = @resource.comments.build
   end
 
+  def create
+    @resource = Resource.find_by(id: params[:resource_id])
+    @comment = @resource.comments.build(comment_params)
+    @comment.user = current_user
+    raise @comment.inspect
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+
 end
