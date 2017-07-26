c542e825f190e898de44b501c12d7017c7da1c8e
Andrew Bonner <andrewbonner92@gmail.com>
2017-07-26 17:55:15 -0400

    Set user on user_resource show page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user
 
   def show
+    if !current_user == @user
+      redirect_to user_resources_path(@user)
+    end
   end
 
+  private
+
+    def set_user
+      @user = User.find_by(id: params[:id])
+    end
 end
