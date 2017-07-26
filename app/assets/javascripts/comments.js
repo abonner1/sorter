df2a291f751afaf301cb193e7fc77629b35bb613
Andrew Bonner <andrewbonner92@gmail.com>
2017-07-26 11:57:38 -0400

    Add basic working comments for with jquery and AJAX.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,19 +1,33 @@
-function Comments(comment) {
-  this.content = comment.content
-  this.user_id = comment.user_id
-  this.resource_id = comment.resource_id
+function Comment(attributes) {
+  this.id = attributes.id
+  this.content = attributes.content
+  this.user_id = attributes.user_id
+  this.resource_id = attributes.resource_id
 }
 
+document.addEventListener("turbolinks:load", function(event) {
+  Comment.templateSource = $("#comment-template").html()
+  Comment.template = Handlebars.compile(Comment.templateSource)
+});
+
+Comment.prototype.renderLI = function () {
+  return Comment.template(this)
+};
+
 $(document).on("submit", "form#new_comment", function (e) {
-  e.preventDefault()
+
   var $form = $(this)
   var action = $form.attr("action")
   var params = $form.serialize()
 
   $.post(action, params, function (json) {
-    console.log(json)
+    var comment = new Comment(json)
+    var commentLI = comment.renderLI()
+
+    $("ul.comments-list").append(commentLI)
   }, "json")
   .error(function (response) {
     console.log("You broke it!", response)
   })
+  e.preventDefault()
 })
