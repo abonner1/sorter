779eed0c4013be88475f46f1e2149b04a895f62f
Andrew Bonner <andrewbonner92@gmail.com>
2017-07-26 14:36:54 -0400

    Fix serializer bug.

--- app/serializers/resource_serializer.rb
+++ app/serializers/resource_serializer.rb
@@ -1,4 +1,4 @@
 class ResourceSerializer < ActiveModel::Serializer
   attributes :id, :title, :url, :description, :user_id
-  has_many :tags, scaerializer: ResourceTagSerializer
+  has_many :tags, serializer: ResourceTagSerializer
 end
