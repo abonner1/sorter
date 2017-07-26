77eac6768bd71a6d2487ad51f35ebc81c40ced8e
Andrew Bonner <andrewbonner92@gmail.com>
2017-07-26 15:43:23 -0400

    Render tags list.

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -2,3 +2,7 @@ function Tag(attributes) {
   this.id = attributes.id
   this.name = attributes.name
 }
+
+Tag.renderLI = function (tag) {
+  return `<li><a href="/tags/${tag.id}">${tag.name}</a></li>`
+}
