77b69d1edb44851e55e83478b92bcfc6d46d9f34
Andrew Bonner <andrewbonner92@gmail.com>
2017-07-27 11:33:03 -0400

    Render language link method js.

--- app/assets/javascripts/languages.js
+++ app/assets/javascripts/languages.js
@@ -2,3 +2,7 @@ function Language(language) {
   this.name = language.name
   this.description = language.description
 }
+
+Language.renderLink = function (language) {
+  return `<a class="lang lang-links" href="/languages/${language.id}">${language.name}</a>`
+}
